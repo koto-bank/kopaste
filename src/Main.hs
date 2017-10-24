30bb9aed6e2ea4cbe7f0bbceb368eea48deed80e
Ekaterina Vaartis <vaartis@cock.li>
2017-10-24 22:47:13 +0300

    Paste clenup
    Pastes should be deleted after a week, checking every hour
    TODO: make configurable?

--- src/Main.hs
+++ src/Main.hs
@@ -2,11 +2,38 @@
 
 module Main where
 
+import Control.Concurrent
+
+import System.Directory
+
 import Control.Monad (msum)
+import Control.Monad.Zip
+
+import Data.Time.Clock
+import Data.Time.Calendar
 
 import Happstack.Server
 
 import Pages
+
+runCleanup :: IO ()
+runCleanup = do
+  pastePaths <- (map $ (++) "pastes/") <$> listDirectory "pastes"
+  modificationTimes <- mapM getModificationTime pastePaths
+  pathTimeList <- return $ zip pastePaths modificationTimes
+  currTime <- getCurrentTime
+  mapM_
+    (\(path, tm) ->
+       let UTCTime dayCurrent _ = currTime
+           UTCTime dayPaste _ = tm
+           pasteDiff = diffDays dayCurrent dayPaste
+       in if pasteDiff >= 7 -- delete after a week
+            then removeFile path
+            else return ())
+    pathTimeList
+  threadDelay 3600000000 -- an hour
+  runCleanup
+
 
 kopasteApp :: ServerPart Response
 kopasteApp =
@@ -19,4 +46,4 @@ kopasteApp =
     ]
 
 main :: IO ()
-main = simpleHTTP nullConf kopasteApp
+main = (forkIO runCleanup) >> simpleHTTP nullConf kopasteApp
